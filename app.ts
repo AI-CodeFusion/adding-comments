ddf1aa013616f353b4b1d367f57a48ebd12b529f
AI-CodeFusion <syed19.freelancer786@gmail.com>
2024-04-22 22:11:31 +0500

    Iadd comments

---
+++ app.ts
@@ -0,0 +1,4 @@
+// exercise 9
+// division
+// Date 22-04-2024
+console.log(100/5);
---
+++ main.js
@@ -0,0 +1,5 @@
+// my name is Benish
+// I am a teacher
+// learning exercise 9
+// Date 22-04-2024
+console.log("my name is Benish and I am a teacher");
